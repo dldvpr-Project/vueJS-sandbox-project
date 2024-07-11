3413c5ab5c38d599c162ca28baf87ec14a8f1bec
Ludovic <dormoy.dev@gmail.com>
2024-07-11 16:24:16 +0200

    add data and first props in compn

--- src/components/Shop/ShopProduct.vue
+++ src/components/Shop/ShopProduct.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import type {ProductInterface} from "@/interfaces/product.interface";
+
+defineProps<{
+  product: ProductInterface
+}>()
+</script>
 
 <template>
   <div class="product d-flex flex-column">
-    <div class="product-image"></div>
+    <div class="product-image" :style="{ backgroundImage: `url(${product.image})` }"></div>
     <div class="p-10 d-flex flex-column">
-      <h4>Macbook Pro</h4>
-      <p>Performances exceptionnelles avec la puce M1 Pro ou M1</p>
+      <h4>{{product.title}}</h4>
+      <p>{{product.description}}</p>
       <div class="d-flex flex-row align-items-center">
-        <strong class="flex-fill">Prix : 1500€</strong>
+        <strong class="flex-fill">Prix : {{product.price}}€</strong>
         <button class="btn btn-primary">Ajouter au panier</button>
       </div>
     </div>
@@ -22,7 +29,7 @@
   &-image {
     border-top-right-radius: var(--border-radius);
     border-top-left-radius: var(--border-radius);
-    background-image: url('../../assets/LD0005898490_1.jpg');
+    background-image: url(src/assets/images/LD0005898490_1.jpg);
     background-size: cover;
     background-position: center;
     height: 250px;
