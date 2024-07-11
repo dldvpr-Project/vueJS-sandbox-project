635f9cae20328ff6aa57c56a602a9f1c990cc756
Ludovic <dormoy.dev@gmail.com>
2024-07-11 12:37:39 +0200

    basic cart layout

--- src/components/Cart/CartProduct.vue
+++ src/components/Cart/CartProduct.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="mb-10 b5">
-    <h1>Product</h1>
+  <div class="mb-10 p-10 d-flex flex-row align-items-center product">
+    <strong class="flex-fill mr-10">Macbook pro</strong>
+    <span class="mr-10">Prix: 1500€</span>
+    <button class="btn btn-danger">Supprimer</button>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product {
+  background-color: var(--gray-1);
+  border: var(--border);
+  border-radius: var(--border-radius);
+}
+</style>
